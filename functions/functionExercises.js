20d025d4cf4389c6e9104a2fdaa87c46117c447c
evaluc <ealuckow@gmail.com>
2024-06-27 15:31:56 -0500

    Make skeleton of functions exercise

---
+++ functions/functionExercises.js
@@ -0,0 +1,29 @@
+
+
+function makeLine(size) {
+
+}
+
+function makeSquare(size) {
+    
+}
+
+function makeRectangle(swidth, height) {
+    
+}
+
+function makeDownwardStairs(height) {
+
+}
+
+function makeSpaceLine(numSpaces, numChars) {
+
+}
+
+function makeIsoscelesTriangle(height) {
+
+}
+
+function makeDiamond(height) {
+    
+}

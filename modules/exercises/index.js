b6e6eb0660bca1ee470f1e49dea6cb88fa958b70
evaluc <ealuckow@gmail.com>
2024-07-11 15:50:29 -0500

    Remove extra constant

--- modules/exercises/index.js
+++ modules/exercises/index.js
@@ -3,7 +3,6 @@ const input = require('readline-sync')//Import readline-sync.
 const averages = require('./ScoreCalcs/averages.js')//Import functions from averages.js.
 const printAll = require('./display.js')//Import function from display.js.
 const randomSelect = require('./randomSelect.js');//Import function from randomSelect.js.
-const randomFromArray = require('./randomSelect.js');
 
 //Candidate data:
 let astronauts = ['Fox','Turtle','Cat','Hippo','Dog'];

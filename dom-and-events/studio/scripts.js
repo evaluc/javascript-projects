d025d809275094c18c5d3038ed57bf3e0b2a5b92
evaluc <ealuckow@gmail.com>
2024-08-05 21:42:33 -0500

    Complete most of Studio 14

--- dom-and-events/studio/scripts.js
+++ dom-and-events/studio/scripts.js
@@ -2,34 +2,36 @@
 // Remember to pay attention to page loading!
 function init () {
 
-    const buttonTakeOff = this.document.getElementbyID("takeoff");
-    const buttonLanding = this.document.getElementbyID("landing");
-    const buttonMissionAbort = this.document.getElementbyID("missionAbort");
+    const buttonTakeOff = this.document.getElementById("takeoff");
+    const buttonLanding = this.document.getElementById("landing");
+    const buttonMissionAbort = this.document.getElementById("missionAbort");
 
-    const buttonUp = this.document.getElementbyID("up");
-    const buttonDown = this.document.getElementbyID("down");
-    const buttonRight = this.document.getElementbyID("right");
-    const buttonLeft = this.document.getElementbyID("left");
+    const buttonUp = this.document.getElementById("up");
+    const buttonDown = this.document.getElementById("down");
+    const buttonRight = this.document.getElementById("right");
+    const buttonLeft = this.document.getElementById("left");
+    const rocket = this.document.getElementById("rocket");
 
-    let flightStatus = this.document.getElementbyID("flightStatus");
-    let shuttleBackgroundColor = this.document.getElementbyID("shuttleBackground");
-    let shuttleHeightMiles = this.document.getElementbyID("spaceShuttleHeight");
+    let flightStatus = this.document.getElementById("flightStatus");
+    let shuttleBackgroundColor = this.document.getElementById("shuttleBackground");
+    let shuttleHeightMiles = this.document.getElementById("spaceShuttleHeight");
+    
 
 
     buttonTakeOff.addEventListener("click", function(event) {
         let response = window.confirm("Confirm that the shuttle is ready for takeoff.");
     
         if(response) {
-            flightStatus.innerHTML = "Shuttle in flight.";
+            flightStatus.innerHTML = ("Shuttle in flight.");
             shuttleBackgroundColor.style.backgroundColor = "blue";
-            shuttleHeightMiles.innerHTML = "10,000";
+            shuttleHeightMiles.innerHTML = ("10,000");
         }
     });
 
     buttonLanding.addEventListener("click", function(event) {
         window.alert("The shuttle is landing. Landing gear engaged.");
 
-        flightStatus.innerHTML = "The Shuttle has landed.";
+        flightStatus.innerHTML = ("The Shuttle has landed.");
         shuttleBackgroundColor.style.backgroundColor = "green";
         shuttleHeightMiles.innerHTML = "0";
     });
@@ -38,11 +40,17 @@ function init () {
         let response = window.confirm("Confirm that you want to abort the mission.");
 
         if (response) {
-            flightStatus.innerHTML
+            flightStatus.innerHTML = ("Mission Aborted.")
             shuttleBackgroundColor.style.backgroundColor = "green";
             shuttleHeightMiles.innerHTML = "0";
         }
+            
+    });
+
+    buttonRight.addEventListener("click", function(event) {
+        rocket.style.right = parseInt(10);
     })
+
 
 }
 
